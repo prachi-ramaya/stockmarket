44757818ae1be33a1f2b0ebb7be7a054c561ca4a
unknown <User@JrJavaFseCogn-1>
2022-04-05 11:26:52 +0530

    kafka initial commit

---
+++ runZookeeper.bat
@@ -0,0 +1,2 @@
+cd C:\Users\User\Downloads\kafka_2.11-2.1.0
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
---
+++ runkafka.bat
@@ -0,0 +1,2 @@
+cd C:\Users\User\Downloads\kafka_2.11-2.1.0
+.\bin\windows\kafka-server-start.bat .\config\server.properties
